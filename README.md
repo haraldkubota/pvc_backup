2507dea6a61da11b6a6eb40268df92b04d0815e5
Harald Kubota <harald.kubota@gmail.com>
2021-07-29 13:10:33 +0900

    Update to use @kubernetes/client-node and use newer local-path directory names

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ This is the result.
 
 This is not a general backup program. It's a solving a specific I had and thus serves more as an example how to do something similar.
 
-* It shuts down the k8s deployment (replicas set to 0)
-* It backs up the local-path PVCs
-* It sets replicas back to 1
+- It shuts down the k8s deployment (replicas set to 0)
+- It backs up the local-path PVCs
+- It sets replicas back to 1
 
 ## Usage
 
@@ -26,7 +26,6 @@ Options:
   -h, --help                   display help for command
 ```
 
-
 ## Example Run
 
 ```
@@ -40,3 +39,8 @@ Running backup for grafana-deployment and volume 2...
 -rw-r--r-- 1 harald users  200 Apr 25 13:40 grafana-log-2021-04-25.tar.xz
 -rw-r--r-- 1 harald users 1.1G Apr 25 13:40 influxdb-data-2021-04-25.tar.xz
 ```
+
+## Update History
+
+The initial version used the very nice kubernetes-client module from GoDaddy but it's not updated for a year.
+So I migrated to the official @kubernetes/client-node module.
--- pvc-backup.js
+++ pvc-backup.js
@@ -1,169 +1,229 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
-const Client = require('kubernetes-client').Client;
-
-const { Command } = require('commander');
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const { Command } = require("commander");
 const program = new Command();
 
+const k8s = require("@kubernetes/client-node");
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
 program
-    .version('1.0.0', '-v, --version')
-    .usage('[OPTIONS]...')
-    .option('-n, --namespace <namespace>', 'The namespace (default is "default")', 'default')
-    .option('-w, --wait N', 'Wait N seconds for the replica count to change (default is 10)', 10)
-    .option('-d, --debug', 'Debug', false)
-    .parse(process.argv);
+  .version("1.0.0", "-v, --version")
+  .usage("[OPTIONS]...")
+  .option(
+    "-n, --namespace <namespace>",
+    'The namespace (default is "default")',
+    "default"
+  )
+  .option("-d, --debug", "Debug", false)
+  .parse(process.argv);
 
-const debug = program.debug;
-const waitTime = program.wait;
+const options = program.opts();
+// console.log(`Options: ${JSON.stringify(options)}`);
+const debug = options.debug;
 
-async function gatherPVCs(client, namespace) {
-    try {
-        let myPVCs=[];
+async function gatherPVCs(kc, namespace) {
+  try {
+    let myPVCs = [];
 
-        // Find deployments and their PVCs
-        const deployments = await client.apis.apps.v1.namespaces(namespace).deployments().get();
-        // See also https://knode4.lan:6443/apis/apps/v1/namespaces/default/deployments
+    const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+    const k8sAppsV1Api = kc.makeApiClient(k8s.AppsV1Api);
 
-        for (let item of deployments.body.items) {
+    // Find deployments and their PVCs
 
-            if (item.spec.template.spec.volumes)
-                for (volumes of item.spec.template.spec.volumes) {
-                    if (volumes.persistentVolumeClaim) {
+    // const deployments = await client.apis.apps.v1.namespaces(namespace).deployments().get();
 
-                        myPVCs.push({
-                            deployment: item.metadata.name,
-                            claimName: volumes.persistentVolumeClaim.claimName
-                        });
-                    }
-                }
+    const deployments = await k8sAppsV1Api.listNamespacedDeployment(namespace);
+
+    if (debug) console.log(deployments);
+    // See also https://knode4.lan:6443/apis/apps/v1/namespaces/default/deployments
+
+    for (let item of deployments.body.items) {
+      if (item.spec.template.spec.volumes)
+        for (volumes of item.spec.template.spec.volumes) {
+          if (volumes.persistentVolumeClaim) {
+            myPVCs.push({
+              deployment: item.metadata.name,
+              claimName: volumes.persistentVolumeClaim.claimName,
+            });
+          }
         }
-        if (debug)
-            console.log(JSON.stringify(myPVCs));
-
-        const pvcs = await client.api.v1.namespaces(namespace).persistentvolumeclaims.get();
-        for (let item of pvcs.body.items) {
-            let ignore=false;
-            if (item.spec.storageClassName !== "local-path") {
-                console.error(`${item.metadata.name}: Cannot handle storage provider ${item.spec.storageClassName}.`);
-                ignore=true;
-            }
-            if (item.status.phase !== "Bound") {
-                console.error(`${item.metadata.name}: Cannot handle PVC which is ${item.status.phase} instead of "Bound"`);
-                ignore = true;
-            }
-            if (item.status.accessModes.length===1 && item.status.accessModes[0] !== "ReadWriteOnce") {
-                console.error(`${item.metadata.name}: Cannot handle PVCs which are ${item.status.accessModes} instead of "ReadWriteOnce"`);
-                ignore = true;
-            }
-            if (!ignore) {
-                let foundIt=false;
-                for (let i in myPVCs) {
-                    if (myPVCs[i].claimName === item.metadata.name) {
-                        foundIt = true;
-                        myPVCs[i].node = item.metadata.annotations["volume.kubernetes.io/selected-node"];
-                        myPVCs[i].path = item.spec.volumeName;
-                    }
-                }
-                if (!foundIt) {
-                    console.error(`PVC ${item.metadata.name} should have existed, but it does not!`);
-                }
-            } else {
-                console.log(`Ignoring ${item.metadata.name}.`);
-            }
-        }
-        return myPVCs;
-    } catch (err) {
-        console.error('Error: ', err);
     }
-}
+    if (debug) console.log(JSON.stringify(myPVCs));
 
+    const pvcs = await k8sCoreV1Api.listNamespacedPersistentVolumeClaim(
+      namespace
+    );
+    if (debug) console.log(JSON.stringify(pvcs));
+
+    for (let item of pvcs.body.items) {
+      let ignore = false;
+      if (item.spec.storageClassName !== "local-path") {
+        console.error(
+          `${item.metadata.name}: Cannot handle storage provider ${item.spec.storageClassName}.`
+        );
+        ignore = true;
+      }
+      if (item.status.phase !== "Bound") {
+        console.error(
+          `${item.metadata.name}: Cannot handle PVC which is ${item.status.phase} instead of "Bound"`
+        );
+        ignore = true;
+      }
+      if (
+        item.status.accessModes.length === 1 &&
+        item.status.accessModes[0] !== "ReadWriteOnce"
+      ) {
+        console.error(
+          `${item.metadata.name}: Cannot handle PVCs which are ${item.status.accessModes} instead of "ReadWriteOnce"`
+        );
+        ignore = true;
+      }
+      if (!ignore) {
+        let foundIt = false;
+        for (let i in myPVCs) {
+          if (myPVCs[i].claimName === item.metadata.name) {
+            foundIt = true;
+            myPVCs[i].node =
+              item.metadata.annotations["volume.kubernetes.io/selected-node"];
+            myPVCs[i].path = item.spec.volumeName;
+          }
+        }
+        if (!foundIt) {
+          console.error(
+            `PVC ${item.metadata.name} should have existed, but it does not!`
+          );
+        }
+      } else {
+        console.log(`Ignoring ${item.metadata.name}.`);
+      }
+    }
+    return myPVCs;
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+}
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-async function setReplicas(client, namespace, deployment, replicaCount) {
-    // Scale deployment to 0 for backup and probably 1 otherwise
-    if (debug) {
-        console.log(`Changing ${deployment} to ${replicaCount} replicas...`)
+async function setReplicas(kc, namespace, deployment, replicaCount) {
+  const k8sAppsV1Api = kc.makeApiClient(k8s.AppsV1Api);
+
+  // Scale deployment to 0 for backup and probably 1 otherwise
+  if (debug)
+    console.log(`Changing ${deployment} to ${replicaCount} replicas...`);
+  else {
+    // See https://github.com/kubernetes-client/javascript/issues/19#issuecomment-582886605
+    const headers = {
+      "content-type": "application/strategic-merge-patch+json",
+    };
+    await k8sAppsV1Api.patchNamespacedDeployment(
+      deployment,
+      namespace,
+      {
+        spec: {
+          replicas: replicaCount,
+        },
+      },
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      { headers }
+    );
+  }
+  if (debug) console.log(`...Done`);
+  // Wait at least 1s and at most 10s
+  else {
+    for (let i = 0; i < 10; ++i) {
+      let state = await k8sAppsV1Api.readNamespacedDeployment(
+        deployment,
+        namespace
+      );
+      let replicas = state.body.spec.replicas;
+      if (debug) console.log(`replicas=${replicas}`);
+      await sleep(1000);
+      if (replicas === replicaCount) break;
     }
-    else {
-        await client.apis.apps.v1.ns(namespace).deployments(deployment).patch({
-            body: {
-                spec: {
-                    replicas: replicaCount
-                }
-            }
-        });
-    }
-    if (debug) console.log(`...Done`);
-    // Wait at least 1s and at most waitTime seconds
-    else {
-        for (let i=0; i<waitTime; ++i) {
-            let state = await client.apis.apps.v1.ns(namespace).deployments(deployment).get();
-            let replicas = state.body.spec.replicas;
-            if (debug) console.log(`replicas=${replicas}`);
-            await sleep(1000);
-            if (replicas === replicaCount) break;
-        }
-    }
+  }
 }
 
 function todayDate() {
-    let a=new Date();
-    let year=a.getFullYear();
-    let month=a.getMonth()+1;
-    let day=a.getDate();
-    return `${year}-${month<10?"0"+month:""+month}-${day<10?"0"+day:""+day}`;
+  let a = new Date();
+  let year = a.getFullYear();
+  let month = a.getMonth() + 1;
+  let day = a.getDate();
+  return `${year}-${month < 10 ? "0" + month : "" + month}-${
+    day < 10 ? "0" + day : "" + day
+  }`;
 }
 
 // noinspection SpellCheckingInspection
-async function doBackup(client, namespace, pvcs) {
-    // Gather deployments as we handle one deployment at a time
-    let deployments=new Set();
+async function doBackup(kc, namespace, pvcs) {
+  // Gather deployments as we handle one deployment at a time
+  let deployments = new Set();
 
-    for (let item of pvcs) {
-        deployments.add(item.deployment);
-    }
+  for (let item of pvcs) {
+    deployments.add(item.deployment);
+  }
 
-    for (let deployment of deployments) {
-        await setReplicas(client, namespace, deployment, 0);
+  for (let deployment of deployments) {
+    await setReplicas(kc, namespace, deployment, 0);
 
-        if (debug) console.log("Running backup jobs:");
-        for (let volume in pvcs) {
-            if (pvcs[volume].deployment === deployment) {
-                // console.log(`ssh ${pvcs[volume].node}.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${pvcs[volume].path} | xz -2 > ${pvcs[volume].claimName}-${todayDate()}.tar.xz`);
-                console.log(`Running backup for ${deployment} and volume ${volume}...`);
-                if (debug) {
-                    console.log(`ssh ${pvcs[volume].node}.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${pvcs[volume].path} | xz -2 > ${pvcs[volume].claimName}-${todayDate()}.tar.xz`);
-                    }
-                else {
-                    const { stdout, stderr } = await exec(`ssh ${pvcs[volume].node}.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${pvcs[volume].path} | xz -2 > ${pvcs[volume].claimName}-${todayDate()}.tar.xz`);
-                    if (debug) {
-                        console.log(`stdout: ${stdout}`);
-                        console.log(`stderr: ${stderr}`);
-                    }
-                }
-                if (debug) {
-                    console.log('...end.');
-                }
-            }
+    if (debug) console.log("Running backup jobs:");
+    for (let volume in pvcs) {
+      if (pvcs[volume].deployment === deployment) {
+        // console.log(`ssh ${pvcs[volume].node}.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${pvcs[volume].path} | xz -2 > ${pvcs[volume].claimName}-${todayDate()}.tar.xz`);
+        console.log(`Running backup for ${deployment} and volume ${volume}...`);
+        if (debug) {
+          console.log(
+            `ssh ${
+              pvcs[volume].node
+            }.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${
+              pvcs[volume].path
+            }_${namespace}_${pvcs[volume].claimName} | xz -2 > ${
+              pvcs[volume].claimName
+            }-${todayDate()}.tar.xz`
+          );
+        } else {
+          const { stdout, stderr } = await exec(
+            `ssh ${
+              pvcs[volume].node
+            }.lan  -- sudo tar -C /var/lib/rancher/k3s/storage -c -f - ${
+              pvcs[volume].path
+            }_${namespace}_${pvcs[volume].claimName} | xz -2 > ${
+              pvcs[volume].claimName
+            }-${todayDate()}.tar.xz`
+          );
+          if (debug) {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+          }
         }
-        await setReplicas(client, namespace, deployment, 1);
+        if (debug) {
+          console.log("...end.");
+        }
+      }
     }
-    // For backup, do the equivalent of:
-    // ssh knode6.lan -- sudo tar -C /var/lib/rancher/k3s/storage -c -j \
-    //   -f - pvc-a24fabf7-9c28-4aa5-a4cf-6ac71
+    await setReplicas(kc, namespace, deployment, 1);
+  }
 }
 
 async function main() {
-    const client = new Client({ version: '1.13' });
-    let pvcs=await gatherPVCs(client, program.namespace);
-    //console.log(JSON.stringify(pvcs, null, "  "));
-    await doBackup(client, program.namespace, pvcs);
+  // Using @kubernetes/client-node is a bit more complex, but here's a working sample:
+  // const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+  //let res = await k8sCoreV1Api.listNamespacedPod('default');
+  //console.log(res.body);
+  //console.log(res.body.items[0].metadata);
+
+  let pvcs = await gatherPVCs(kc, options.namespace);
+  console.log(JSON.stringify(pvcs, null, "  "));
+  await doBackup(kc, options.namespace, pvcs);
 }
 
 main();
